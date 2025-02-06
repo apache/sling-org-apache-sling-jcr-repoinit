4237a6fa197fbae3837ba815c680bbd4127fae5e
Joerg Hoh <joerghoh@apache.org>
2025-02-06 10:21:44 +0100

    restructure the modifications to SlingOptions, add some more docs

--- src/test/java/org/apache/sling/jcr/repoinit/it/RepoInitTestSupport.java
+++ src/test/java/org/apache/sling/jcr/repoinit/it/RepoInitTestSupport.java
@@ -29,6 +29,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.ops4j.pax.exam.Configuration;
 import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.options.ModifiableCompositeOption;
 import org.ops4j.pax.exam.util.PathUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -54,67 +55,53 @@ public abstract class RepoInitTestSupport extends TestSupport {
     protected static final Logger log = LoggerFactory.getLogger(RepoInitTestSupport.class.getName());
 
     final String repositoriesURL = "https://repo1.maven.org/maven2@id=central";
-    
-    // artifact ID of all oak bundles
+
+    private static final String JR_GID = "org.apache.jackrabbit";
+
+    // artifact ID of all relevant oak bundles
     private static final String[] OAK_BUNDLES = new String[] {"oak-api","oak-blob",
             "oak-blob-plugins","oak-commons","oak-core","oak-core-spi","oak-jcr",
             "oak-lucene","oak-query-spi","oak-security-spi","oak-segment-tar","oak-store-composite",
             "oak-store-document","oak-store-spi", "oak-jackrabbit-api"};
-    
+    private static final String TARGET_OAK_VERSION = "1.44.0";
+
     private static final String[] JACKRABBIT_2x_BUNDLES = new String[] {"jackrabbit-data","jackrabbit-jcr-commons",
             "jackrabbit-jcr-rmi","jackrabbit-spi-commons","jackrabbit-spi","jackrabbit-webdav"};
-  
+    private static final String TARGET_JACKRABBIT_VERSION = "2.22.0";
+
     @Inject
     private SlingRepository repository;
 
     @Configuration
     public Option[] configuration() {
-        
-        updateOakVersion("1.44.0");
-        Collection<Option> nonExistingJackrabbitBundles = updateJackrabbit("2.22.0");
-        SlingOptions.versionResolver.setVersion("org.apache.sling", "org.apache.sling.jcr.oak.server", "1.2.10");
+
+        // overwrite provided versions
+        Collection<Option> nonExistingJackrabbitBundles = updateRepositoryBundleVersions();
         SlingOptions.versionResolver.setVersionFromProject("org.apache.sling", "org.apache.sling.repoinit.parser");
         SlingOptions.versionResolver.setVersionFromProject("org.apache.sling", "org.apache.sling.commons.metrics");
         SlingOptions.versionResolver.setVersionFromProject("org.apache.commons", "commons-lang3"); // for the metrics
+
         return options(composite(
                         super.baseConfiguration(),
                         vmOption(System.getProperty("pax.vm.options")),
                         slingQuickstart(),
+                        junitBundles(),
+                        SlingOptions.logback(),
+                        systemProperty("logback.configurationFile")
+                            .value("file:" + PathUtils.getBaseDir() + "/src/test/resources/logback-it.xml"),
+                        awaitility(),
                         testBundle("bundle.filename"),
+                        newConfiguration("org.apache.sling.jcr.base.internal.LoginAdminWhitelist")
+                            .put("whitelist.bundles.regexp", "^PAXEXAM.*$")
+                            .asOption(),
                         mavenBundle()
                                 .groupId("org.apache.sling")
                                 .artifactId("org.apache.sling.repoinit.parser")
                                 .versionAsInProject(),
-                        mavenBundle()
-                            .groupId("org.apache.jackrabbit")
-                            .artifactId("oak-jackrabbit-api")
-                            .versionAsInProject(),
-                        mavenBundle()
-                            .groupId("org.apache.jackrabbit")
-                            .artifactId("oak-store-spi")
-                            .versionAsInProject(),
-                        mavenBundle()
-                            .groupId("commons-codec")
-                            .artifactId("commons-codec")
-                            .version("1.14"),
-                        mavenBundle()
-                            .groupId("commons-io")
-                            .artifactId("commons-io")
-                            .version("2.11.0"),
-                        mavenBundle()
-                            .groupId("org.apache.tika")
-                            .artifactId("tika-core")
-                            .version("1.24.1"),
-                        SlingOptions.logback(),
-                        systemProperty("logback.configurationFile").value("file:" + PathUtils.getBaseDir() + "/src/test/resources/logback-it.xml"),
-                        junitBundles(),
-                        awaitility(),
-                        newConfiguration("org.apache.sling.jcr.base.internal.LoginAdminWhitelist")
-                                .put("whitelist.bundles.regexp", "^PAXEXAM.*$")
-                                .asOption())
+                        bundlesForNewerRepository()
+                        )
                 .add(additionalOptions())
                 .remove(nonExistingJackrabbitBundles)
-                .remove(mavenBundle().groupId("org.apache.jackrabbit").artifactId("jackrabbit-api").version(versionResolver))
                 .remove(
                         // remove our bundle under test to avoid duplication
                         mavenBundle()
@@ -123,34 +110,49 @@ public abstract class RepoInitTestSupport extends TestSupport {
                                 .version(versionResolver)));
     }
 
-    protected void updateOakVersion(String defaultVersion) {
+    /**
+     * Update the version number of all relevant bundles to run a newer Oak/Jackrabbit repository; in case that
+     * bundle does not exist in the requested version, it is ignored and added to collection returned.
+     * @return the list of bundles which must be removed from the list of bundles.
+     */
+    private static Collection<Option> updateRepositoryBundleVersions() {
+        SlingOptions.versionResolver.setVersion("org.apache.sling", "org.apache.sling.jcr.oak.server", "1.2.10");
         for (int i=0;i< OAK_BUNDLES.length;i++) {
             try {
-                SlingOptions.versionResolver.setVersionFromProject("org.apache.jackrabbit", OAK_BUNDLES[i]);
+                SlingOptions.versionResolver.setVersionFromProject(JR_GID, OAK_BUNDLES[i]);
             } catch (IllegalArgumentException e) {
                 // the version is not explicitly listed in the pom, so use the provided default version
-                SlingOptions.versionResolver.setVersion("org.apache.jackrabbit", OAK_BUNDLES[i], defaultVersion);
+                versionResolver.setVersion(JR_GID, OAK_BUNDLES[i], TARGET_OAK_VERSION);
             }
         }
-    }
-    
-    protected Collection<Option> updateJackrabbit(String version) {
         Collection<Option> nonExistingBundles = new HashSet<>();
         for (int i=0;i< JACKRABBIT_2x_BUNDLES.length;i++) {
             Option existingBundle = null;
             try {
                 String artifactId=JACKRABBIT_2x_BUNDLES[i];
-                existingBundle = mavenBundle().groupId("org.apache.jackrabbit").artifactId(artifactId).versionAsInProject();
-                SlingOptions.versionResolver.setVersion("org.apache.jackrabbit", artifactId, version);
+                existingBundle = mavenBundle().groupId(JR_GID).artifactId(artifactId).versionAsInProject();
+                SlingOptions.versionResolver.setVersion(JR_GID, artifactId, TARGET_JACKRABBIT_VERSION);
             } catch (IllegalArgumentException e) {
                 nonExistingBundles.add(existingBundle);
-                
             }
         }
         return nonExistingBundles;
     }
-    
-    
+
+    /**
+     * add relevant bundles which are required to run the newer repository version
+     * @return
+     */
+    private static ModifiableCompositeOption bundlesForNewerRepository() {
+        return composite(
+                mavenBundle().groupId(JR_GID).artifactId("oak-jackrabbit-api").version(SlingOptions.versionResolver),
+                mavenBundle().groupId(JR_GID).artifactId("oak-store-spi").version(SlingOptions.versionResolver),
+                mavenBundle().groupId("commons-codec").artifactId("commons-codec").version("1.14"),
+                mavenBundle().groupId("commons-io").artifactId("commons-io").version("2.11.0"),
+                mavenBundle().groupId("org.apache.tika").artifactId("tika-core").version("1.24.1"),
+                mavenBundle().groupId(JR_GID).artifactId("jackrabbit-api").version(versionResolver)
+                );
+    }
     
     protected Option[] additionalOptions() {
         return new Option[] {};
